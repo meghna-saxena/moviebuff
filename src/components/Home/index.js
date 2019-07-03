1d230d6f4c05afca9eba13b61e8a0f81c6f005c6
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-07-03 17:43:48 +0200

    scaffolding components for home page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import HeroImage from '../elements/HeroImage';
+import SearchBar from '../elements/SearchBar';
+import FourColGrid from '../elements/FourColGrid';
+import MovieThumb from '../elements/MovieThumb'
+import LoadMoreBtn from '../elements/LoadMoreBtn';
+import Spinner from '../elements/Spinner';
 import './Home.css';
 
 class Home extends React.Component {
@@ -8,7 +14,13 @@ class Home extends React.Component {
 
     render() {
         return (
-           <div>Home</div> 
+            <div className="rmdb-home">
+                <HeroImage />
+                <SearchBar />
+                <FourColGrid />
+                <Spinner />
+                <LoadMoreBtn />
+            </div>
         )
     }
 }
---
+++ src/components/elements/SearchBar/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                SearchBar
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
