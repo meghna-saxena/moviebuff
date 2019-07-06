7595d655e67ba8dc1d37611d2079ccdcc0e7f0f5
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-07-06 12:32:17 +0200

    created actor component

--- src/components/elements/Actor/index.js
+++ src/components/elements/Actor/index.js
@@ -3,9 +3,20 @@ import * as config from '../../../config';
 import './Actor.css';
 
 const Actor = (props) => {
+    const POSTER_SIZE = w154;
+    const actorImage = props.actor.profile_path
+        ? `${config.IMAGE_BASE_URL}${config.POSTER_SIZE}${props.actor.profile_path}`
+        : './images/no_image.jpg';
+
     return (
-        <div>
-            Actor
+        <div className="rmdb-actor">
+            <img src={actorImage} alt="actorthumb" />
+            <span className="rmdb-actor-name">
+                {props.actor.name}
+            </span>
+            <span className="rmdb-actor-character">
+            {props.actor.character}
+        </span>
         </div>
     )
 }
